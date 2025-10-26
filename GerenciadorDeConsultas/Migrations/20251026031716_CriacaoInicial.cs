6c7d921a41fb9dc06e349b6b1c330c82ef873b86
SirPablo17 <pablo.rosario2019@gmail.com>
2025-10-26 00:21:26 -0300

    feat: adicionando migration.

---
+++ GerenciadorDeConsultas/Migrations/20251026031716_CriacaoInicial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorDeConsultas.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Procedimentos",
+                columns: table => new
+                {
+                    ProcedimentoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ValorProcedimento = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TempoProcedimento = table.Column<int>(type: "int", nullable: false),
+                    DescricaoProcedimento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedimentos", x => x.ProcedimentoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroCasa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenhaUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsuarioAtivoInativo = table.Column<bool>(type: "bit", nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuantidadeTentativas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    ConsultaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ValorConsulta = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataConsulta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConsultaConfirmada = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.ConsultaID);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Usuarios_UsuarioID",
+                        column: x => x.UsuarioID,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConsultaProcedimento",
+                columns: table => new
+                {
+                    ConsultasConsultaID = table.Column<int>(type: "int", nullable: false),
+                    ProcedimentosProcedimentoID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConsultaProcedimento", x => new { x.ConsultasConsultaID, x.ProcedimentosProcedimentoID });
+                    table.ForeignKey(
+                        name: "FK_ConsultaProcedimento_Consultas_ConsultasConsultaID",
+                        column: x => x.ConsultasConsultaID,
+                        principalTable: "Consultas",
+                        principalColumn: "ConsultaID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConsultaProcedimento_Procedimentos_ProcedimentosProcedimentoID",
+                        column: x => x.ProcedimentosProcedimentoID,
+                        principalTable: "Procedimentos",
+                        principalColumn: "ProcedimentoID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsultaProcedimento_ProcedimentosProcedimentoID",
+                table: "ConsultaProcedimento",
+                column: "ProcedimentosProcedimentoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_UsuarioID",
+                table: "Consultas",
+                column: "UsuarioID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConsultaProcedimento");
+
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Procedimentos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
